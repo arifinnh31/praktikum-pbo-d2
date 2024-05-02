6dd7fa40370861bf8ad7f08be10fde3e81ecee2e
Arifin N.H <arifinnh311@gmail.com>
2024-05-02 18:07:48 +0700

    pertemuan 7 modul

---
+++ pertemuan-07/modul/10b/inclusion/Bus.java
@@ -0,0 +1,9 @@
+/* 
+ * File      : Bus.java    02/05/24
+ * Penulis   : Arifin Nurmuhammad Haris
+ * Deskripsi : Subclass Bus dari Superclass Vehicle
+ */
+
+public class Bus extends Vehicle {
+
+}
